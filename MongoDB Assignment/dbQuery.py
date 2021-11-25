d9a1ec29df5ee5beceaef064b0f64effddd0bdfe
Saul Burgess <Burgesssaul@gmail.com>
2021-11-25 11:45:17 +0000

    Finishes selects, working on agregation

--- MongoDB Assignment/dbQuery.py
+++ MongoDB Assignment/dbQuery.py
@@ -4,5 +4,27 @@ mongo_client = MongoClient()
 db = mongo_client.crashDB
 col = db["relationalImport"]
 
+print("Demonstrate access\n")
 for x in col.find({ "Aircraft": "Farman F-60 Goliath" }):
+    print(x) 
+    print("\n")
+input("Press enter to continue.")
+
+
+print("\n\nDemonstrate access of sub document\n")
+for x in col.find({ "Crashes.Location": "Pemberville, Ohio"}):
     print(x)
+    print("\n")
+input("Press enter to continue.")
+
+
+print("\n\nDemonstrate sort and aggregate\n")
+for x in col.aggregate([
+    { "$match": {"Crashes.Casulaties.Aboard": "2"}},
+    { "$sort": {"Crashes.Casulaties.Aboard" : -1}}
+    ]):
+
+    print(x)
+    print("\n")
+input("Press enter to continue.")
+
